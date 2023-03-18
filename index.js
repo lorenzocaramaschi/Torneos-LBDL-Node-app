595bcded966d5c899d46e441a59e22d7bcda9f03
Lorenzo <caramaschilolo@gmail.com>
2023-03-18 05:30:11 -0300

    Added Tournaments window, Tournaments info window and creating a Tournament and Match in /admin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Mongoose, Schema } from "mongoose";
 import bodyParser from "body-parser";
 import express from "express";
 import { fileURLToPath } from "url";
@@ -24,7 +24,43 @@ const teamSchema = new Schema(
   }
 );
 
-const Team = mongoose.model("Team", teamSchema);
+const tournamentSchema = new Schema(
+  {
+    logo: String,
+    name: String,
+    year: Number,
+    playlist: String,
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const matchSchema = new Schema(
+  {
+    video: String,
+    home: Schema.Types.ObjectId,
+    away: Schema.Types.ObjectId,
+    tournament: Schema.Types.ObjectId,
+    round: String,
+    homeScore: Number,
+    awayScore: Number,
+    penalties: Boolean,
+    penaltiesHome: Number,
+    penaltiesAway: Number,
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Team = mongoose.model("Team", teamSchema, "teams");
+const Tournament = mongoose.model(
+  "Tournament",
+  tournamentSchema,
+  "tournaments"
+);
+const Match = mongoose.model("Match", matchSchema, "matches");
 
 const app = express();
 
@@ -54,14 +90,45 @@ app.post("/admin", function (req, res) {
       console.log(err);
     });
 });
-app.delete("/admin", function (req, res) {
-  Team.deleteOne({ name: req.body.teamName }, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("User deleted successfully!");
-    }
+
+app.post("/admin/tournaments", (req, res) => {
+  const newTournament = new Tournament({
+    logo: req.body.tournamentLogo,
+    name: req.body.tournamentName,
+    year: req.body.tournamentYear,
+    playlist: req.body.tournamentPlaylist,
   });
+  newTournament
+    .save()
+    .then(() => {
+      res.render("admin");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/admin/matches", (req, res) => {
+  const newMatch = new Match({
+    video: req.body.video,
+    home: req.body.home,
+    away: req.body.away,
+    tournament: req.body.tournament,
+    round: req.body.round,
+    homeScore: req.body.homeScore,
+    awayScore: req.body.awayScore,
+    penalties: req.body.penalties,
+    penaltiesHome: req.body.penaltiesHome,
+    penaltiesAway: req.body.penaltiesAway,
+  });
+  newMatch
+    .save()
+    .then(() => {
+      res.render("admin");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/", (req, res) => {
@@ -75,13 +142,30 @@ app.get("/equipos", async (req, res) => {
 
 app.listen(3000, function () {
   console.log("App is running on Port 3000");
-  console.log(__dirname);
 });
 
 app.get("/equipos/:team", async (req, res) => {
   const teams = await Team.find({});
-  const {team} = req.params;
-  const teamInfo = await Team.find({teamName: {$eq: team}})  
-  
-  res.render("infoEquipos", { teams: teams, team: team, teamInfo: teamInfo });
+  const { team } = req.params;
+  const teamInfo = await Team.find({ teamName: { $eq: team } });
+  const matches =  await Match.find({$or: [{home:{$eq: team}},{away: {$eq: team}}]});
+
+  res.render("infoEquipos", { teams: teams, team: team, teamInfo: teamInfo, matches:matches });
+});
+
+app.get("/torneos", async (req, res) => {
+  const tournaments = await Tournament.find({});
+  res.render("torneos", { tournaments: tournaments });
+});
+
+app.get("/torneos/:tournament", async (req, res) => {
+  const tournaments = await Tournament.find({});
+  const { tournament } = req.params;
+  const tournamentInfo = await Tournament.find({ name: { $eq: tournament } });  
+
+  res.render("infoTorneos", {
+    tournaments: tournaments,
+    tournament: tournament,
+    tournamentInfo: tournamentInfo,
+  });
 });
